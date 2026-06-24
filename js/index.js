f2ef22a795dad15f41a47aafe377e07fd8ce7ea3
moi <187478441+moi-7@users.noreply.github.com>
2026-06-24 14:59:24 -0600

    Refactor:  Formulario de Iniciar sesión
    Uso de plantilla de HTML y CSS para formulario de inicio de sesión

--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
-const root = document.querySelector("div");
-const h1 = document.createElement("h1");
 
-
-h1.textContent = "Element added via javascript";
-h1.classList.add("heading");
-root.appendChild(h1);
