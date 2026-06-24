62e45a1eeff82f7bcf175aaeccf549b450899ede
moi <187478441+moi-7@users.noreply.github.com>
2026-06-24 11:29:42 -0600

    Refactor: Estilos base
    Reinicio de los estilos por defecto del navegador

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
-const body = document.querySelector("body");
-const root = document.createElement("div");
-
-body.appendChild(root);
-root.setAttribute("id", "root");
-
+const root = document.querySelector("div");
 const h1 = document.createElement("h1");
-h1.textContent = "testing...";
-h1.classList.add("test");
-root.appendChild(h1);
 
+
+h1.textContent = "Element added via javascript";
+h1.classList.add("heading");
+root.appendChild(h1);
